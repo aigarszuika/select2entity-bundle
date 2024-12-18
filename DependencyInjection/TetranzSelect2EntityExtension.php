bdd1742779eaae5af7e131f01a61162c7c5f4fec
aigarszuika <aigars.zuika@gmail.com>
2024-12-18 14:17:35 +0200

    PHP 8.4 and Symfony 7 support

--- DependencyInjection/TetranzSelect2EntityExtension.php
+++ DependencyInjection/TetranzSelect2EntityExtension.php
@@ -4,7 +4,7 @@ namespace Tetranz\Select2EntityBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Extension\Extension ;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
